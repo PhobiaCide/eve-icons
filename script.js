88f5742b12bb3772643ef871502cd992a3aa3131
PhobiaCide <80663955+PhobiaCide@users.noreply.github.com>
2023-06-04 12:20:05 -0500

    make  arbitrary aesthetic choices

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function darkMode() {
-  const toggleButton = document.querySelector('#dark-light-toggle .button-front');
+  const toggleButton = document.querySelector(
+    "#dark-light-toggle .button-front"
+  );
   const body = document.body;
 
   const buttonText = toggleButton.innerText;
-  toggleButton.innerText = (buttonText === 'dark mode') ? 'light mode' : 'dark mode';
+  toggleButton.innerText = buttonText === "dark" ? "light" : "dark";
 
-  body.classList.toggle('dark-mode');
+  body.classList.toggle("dark-mode");
 }
